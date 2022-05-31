d80582d3cfa04188f296765da09472315c163058
abyy00001@stud.uni-saarland.de <abhikumar271188@gmail.com>
2022-05-31 23:07:09 +0200

    commit

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
+from distutils.command.upload import upload
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=200, null=True, blank=True)
+    profile_image = models.ImageField(default='profiles/user-default.png', 
+    blank=True, null=True, upload_to='profiles/')
+    short_intro = models.CharField(max_length=200, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    social_github = models.CharField(max_length=200, blank=True, null=True)
+    social_twitter = models.CharField(max_length=200, blank=True, null=True)
+    social_linkedin = models.CharField(max_length=200, blank=True, null=True)
+    social_youtube = models.CharField(max_length=200, blank=True, null=True)
+    social_webiste = models.CharField(max_length=200, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.user.username)
