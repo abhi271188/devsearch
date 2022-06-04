dde0b934d5f1b3bd3cd433ac24ec78173cc69c8e
abyy00001@stud.uni-saarland.de <abhikumar271188@gmail.com>
2022-06-04 23:09:25 +0200

    commit

--- users/views.py
+++ users/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from .models import Profile
+from .models import Profile, Skill
 
 def Profiles(request):
     profiles = Profile.objects.all()
-    context = {'profiles' : profiles}
+    skills = Skill.objects.all()
+    context = {'profiles' : profiles, 'skills' : skills}
     return render(request, 'users/profiles.html', context)
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    context = {'profile' : profile}
+    return render(request, 'users/profile.html', context)
 
 
