14fb97fad1cfbbaf1a8c7aeadbd3764204eb42dc
abyy00001@stud.uni-saarland.de <abhikumar271188@gmail.com>
2022-06-11 23:10:01 +0200

    commit

--- users/views.py
+++ users/views.py
@@ -9,7 +9,11 @@ def Profiles(request):
 
 def userProfile(request, pk):
     profile = Profile.objects.get(id=pk)
-    context = {'profile' : profile}
+    # the below code removes those skills that doesn't have any description in a spcific profile.
+    top_skills = profile.skill_set.exclude(description__exact = '')
+    # the below code filter those skills that doesn't have any description in a specific profile.
+    other_skills = profile.skill_set.filter(description = '')
+    context = {'profile' : profile, 'top_skills' : top_skills, 'other_skills' : other_skills}
     return render(request, 'users/profile.html', context)
 
 
