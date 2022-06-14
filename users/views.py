6a010efbc39f5f24fc81a3e1c34101acb8203dfe
abyy00001@stud.uni-saarland.de <abhikumar271188@gmail.com>
2022-06-14 22:42:13 +0200

    commit

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Profile, Skill
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
 
 def Profiles(request):
     profiles = Profile.objects.all()
@@ -20,6 +22,19 @@ def userProfile(request, pk):
     return render(request, 'users/profile.html', context)
 
 def userLogin(request):
+    if request.method == 'POST':
+        username = request.POST[username]
+        password = request.POST[password]
+        try:
+            user = User.objects.get(username = username)
+        except:
+            print('Username does not exist')
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            print('Username/Password does not exist')
     context = {}
     return render(request, 'users/login_register.html', context)
 
