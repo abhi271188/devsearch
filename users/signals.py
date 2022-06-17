f49ec525edea665658ac58cea31ead22f5431063
abyy00001@stud.uni-saarland.de <abhikumar271188@gmail.com>
2022-06-17 23:21:30 +0200

    commit

--- users/signals.py
+++ users/signals.py
@@ -1,4 +1,7 @@
 import email
+import profile
+from unicodedata import name
+from venv import create
 from django.db.models.signals import post_save, post_delete
 from .models import Profile
 from django.contrib.auth.models import User
@@ -28,9 +31,20 @@ def createProfile(sender, instance, created, **kwargs):
             name = user.first_name,
         )
 
+def updateProfile(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+    if created == False:
+        user.first_name = profile.name
+        user.email = profile.email
+        user.username = profile.username
+        user.save()
+
+
 def deleteUser(sender, instance, **kwargs):
     user = instance.user
     user.delete()
 
 post_save.connect(createProfile, sender=User)
+post_save.connect(updateProfile, sender=Profile)
 post_delete.connect(deleteUser, sender=Profile)
