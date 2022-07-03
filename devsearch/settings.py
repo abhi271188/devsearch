7430d74a339b2eb9cc7877b52c3968980147c84f
abyy00001@stud.uni-saarland.de <abhikumar271188@gmail.com>
2022-07-03 23:49:39 +0200

    commit

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -109,7 +109,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'cse.6201@gmail.com'
-EMAIL_HOST_PASSWORD = '123@abhina'
+EMAIL_HOST_PASSWORD = 'hexkutzcjonrxeww'
 
 
 # Static files (CSS, JavaScript, Images)
