20d0b2055cbfb1d9340edfe5b993c95b1d6e07de
abyy00001@stud.uni-saarland.de <abhikumar271188@gmail.com>
2022-07-03 23:20:35 +0200

    commit

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -103,6 +103,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Configuration for sending welcome emails when a user is created
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'cse.6201@gmail.com'
+EMAIL_HOST_PASSWORD = '123@abhina'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
