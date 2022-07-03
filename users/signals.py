080aa7ad2229dc08ec9474e6067f3a9089a2fe7d
abyy00001@stud.uni-saarland.de <abhikumar271188@gmail.com>
2022-07-03 23:27:49 +0200

    commit

--- users/signals.py
+++ users/signals.py
@@ -6,6 +6,9 @@ from django.db.models.signals import post_save, post_delete
 from .models import Profile
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
+
 '''
 #@receiver(post_save, sender = Profile)
 def profileUpdated(sender, instance, created, **kwargs):
@@ -30,6 +33,16 @@ def createProfile(sender, instance, created, **kwargs):
             email = user.email,
             name = user.first_name,
         )
+
+        subject = 'Welcome to the DevSearch'
+        message = 'We are glad you are here!'
+
+        send_mail (
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently = False)
 
 def updateProfile(sender, instance, created, **kwargs):
     profile = instance
