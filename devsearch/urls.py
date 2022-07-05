837b5b29ca2f30ccbaefb0437acb119402652f40
abyy00001@stud.uni-saarland.de <abhikumar271188@gmail.com>
2022-07-05 23:28:10 +0200

    commit

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -14,9 +14,11 @@ urlpatterns = [
     # set the urls for Password Reset with the class based views
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), 
     name='reset_password'),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), 
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='reset_password_sent.html')
+    , name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name = 'reset.html'), 
+    name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='reset_password_complete.html'), 
     name='password_reset_complete'),
 ]
 
